887f0711a388ba43a595cac41d703f71bb6e870f
JMarts32 <j.santosa@uniandes.edu.co>
2023-04-21 09:58:42 -0500

    Pipeline changes, Clean texto class

---
+++ API/cleanText.py
@@ -0,0 +1,24 @@
+import nltk
+nltk.download('stopwords')
+from sklearn.base import BaseEstimator, TransformerMixin
+from nltk.tokenize import wordpunct_tokenize
+from nltk.corpus import stopwords
+import pandas as pd
+from nltk.stem import SnowballStemmer
+
+class cleanText(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.stop_words = set(stopwords.words('spanish'))
+        self.stop_words = list(self.stop_words)
+        self.stop_words.extend([u'.', u'[', ']', u',', u';', u'', u')', u'),', u' ', u'('])
+        self.snowball_stemmer = SnowballStemmer('spanish')
+
+    def fit (self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        return self.clean(X)
+    
+    def clean(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.apply(lambda x: ' '.join([self.snowball_stemmer.stem(word.lower()) for word in wordpunct_tokenize(x) if (word.isalpha() and word.lower() not in self.stop_words)]))
+        return df
--- API/prediction_model.py
+++ API/prediction_model.py
@@ -1,9 +1,11 @@
 import pickle
+from cleanText import cleanText
 import joblib as jb
 
 class PredictionModel:
     def __init__(self):
         self.model = pickle.load(open('modelo_1.pkl', 'rb'))
+        self.vectorizer = jb.load('./data/count_vectorizer.pkl')
         
     def make_prediction(self, data):
         result = self.model.predict(data)
