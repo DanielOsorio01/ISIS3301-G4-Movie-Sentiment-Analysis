a43627116287a75b8c463c312e675ddc4f74e18d
JMarts32 <j.santosa@uniandes.edu.co>
2023-04-25 09:01:10 -0500

    New model that is trained with all of the trainnign data

--- API/prediction_model.py
+++ API/prediction_model.py
@@ -4,7 +4,7 @@ import joblib as jb
 
 class PredictionModel:
     def __init__(self):
-        self.model = pickle.load(open('./data/model.pkl', 'rb'))
+        self.model = pickle.load(open('./data/prediccion_reviews.pkl', 'rb'))
         self.vectorizer = jb.load('./data/count_vectorizer.pkl')
         
     def make_prediction(self, data):
